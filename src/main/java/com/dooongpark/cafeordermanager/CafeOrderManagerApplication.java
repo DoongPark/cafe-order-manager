38c97f83baa951bb151bebb0b2ae5b93bc539ff6
doongpark <dooongpark@gmail.com>
2023-04-24 00:05:29 +0900

    Init project

---
+++ src/main/java/com/dooongpark/cafeordermanager/CafeOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dooongpark.cafeordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeOrderManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dooongpark/cafeordermanager/CafeOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dooongpark.cafeordermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeOrderManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
